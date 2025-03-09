3038f463cd0c516d3943ebc737307c50cd6116d3
qiqiwww <bzhdhdhd825@gmail.com>
2025-03-09 20:33:56 +0200

    Add files via upload

---
+++ lab2.2/lab2.2.cs
@@ -0,0 +1,42 @@
+﻿namespace lab2._2
+{
+    internal class Program
+    {
+
+        static double Factorial(int n)
+        {
+            double rez = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                rez *= i;
+            }
+            return rez;
+        }
+        static double Obchislennya(double x, double meja = 1e-6) {
+            double sumaryada = 0;
+            double chlenryada=0;
+            int nomerchlena = 1;
+
+            while (Math.Abs(chlenryada) <= meja)  
+            {  
+                chlenryada = Math.Sin(Math.PI * nomerchlena / 4) * Math.Sqrt(Math.Pow(2, nomerchlena)) / Factorial(nomerchlena) * Math.Pow(x, nomerchlena);
+               
+                sumaryada += chlenryada;
+                nomerchlena++;
+            }
+            return sumaryada;
+
+        }
+        static void Main()
+        {
+            double[] znachennyaX = { Math.PI / 4, Math.PI, Math.PI * -5.5 };
+
+            foreach (double x in znachennyaX) {
+                double sumaryada = Obchislennya(x);
+                double y = Math.Exp(x)*Math.Sin(x);
+
+                Console.WriteLine($"x = {x:F4},Сума рядка: {sumaryada:F6}, Точне значення: {y:F6}");
+            }
+        }
+    }
+}
