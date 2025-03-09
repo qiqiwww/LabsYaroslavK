93cee7196fd4db50fb82381acbadd1243bc666e7
qiqiwww <bzhdhdhd825@gmail.com>
2025-03-09 20:33:23 +0200

    Add files via upload

---
+++ lab2.1/lab2.1.cs
@@ -0,0 +1,22 @@
+﻿namespace lab2._1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double x = 0,a=0,y=0, krok =0.1, minx=-2,maxx=2;
+
+            Console.WriteLine("x \t a \t y");
+
+            for (x= minx; x <= maxx; x += krok)
+            {
+                for (a = 1.25; a <= 2; a += 0.25)
+                {
+                    y = (1 - Math.Exp(-Math.Pow(x / a, 2))) / a;
+                    Console.WriteLine($"{x:F2} \t {a:F2} \t {y:F4}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
