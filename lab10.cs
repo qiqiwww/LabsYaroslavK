435f60f9446ec5ad320480b88ed3e57e88df1771
qiqiwww <bzhdhdhd825@gmail.com>
2025-05-30 07:29:18 +0300

    Add files via upload

---
+++ lab10.cs
@@ -0,0 +1,52 @@
+﻿namespace lab10
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text;
+
+    class Platizh
+    {
+        public string RahunokPlantyka { get; set; }    
+        public string RahunokOtrymuvacha { get; set; } 
+        public DateTime DataOplaty { get; set; }   
+        public decimal Suma { get; set; }         
+
+        public override string ToString() =>
+            $"Дата: {DataOplaty:dd.MM.yyyy}  Платник: {RahunokPlantyka}  Отримувач: {RahunokOtrymuvacha}  Сума: {Suma:C}";
+    }
+
+    class Programa
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var platezhi = new List<Platizh>
+        {
+            new Platizh { RahunokPlantyka = "123456", RahunokOtrymuvacha = "654321", DataOplaty = new DateTime(2023, 05, 10), Suma = 1500.75m },
+            new Platizh { RahunokPlantyka = "111222", RahunokOtrymuvacha = "333444", DataOplaty = new DateTime(2023, 05, 15), Suma = 800.00m },
+            new Platizh { RahunokPlantyka = "555666", RahunokOtrymuvacha = "777888", DataOplaty = new DateTime(2023, 06, 01), Suma = 2500.50m },
+            new Platizh { RahunokPlantyka = "999000", RahunokOtrymuvacha = "111000", DataOplaty = new DateTime(2023, 06, 20), Suma = 950.25m }
+        };
+
+            Console.Write("Початкова дата періоду (дд.мм.рррр): ");
+            DateTime pochatkovaData = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            Console.Write("Кінцева дата періоду (дд.мм.рррр): ");
+            DateTime kincevaData = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            Console.Write("Мінімальна сума платежу: ");
+            decimal minimalniyPlatizh = decimal.Parse(Console.ReadLine());
+
+            var filteredPayments = platezhi
+                .Where(p => p.DataOplaty >= pochatkovaData && p.DataOplaty <= kincevaData && p.Suma >= minimalniyPlatizh);
+
+            Console.WriteLine("\nЗнайдені платежі:");
+            foreach (var payment in filteredPayments)
+            {
+                Console.WriteLine(payment);
+            }
+        }
+    }
+}
---
+++ lab7(3).cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+
+namespace lab7
+{
+    public interface ICipher
+    {
+        string encode(string input);
+        string decode(string input);
+    }
+
+    public class ACipher : ICipher
+    {
+        private const string AlphabetUpper = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";
+        private const string AlphabetLower = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+
+        public string encode(string input)
+        {
+            return Transform(input, shift: 1);
+        }
+
+        public string decode(string input)
+        {
+            return Transform(input, shift: -1);
+        }
+
+        private string Transform(string input, int shift)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                int upperIndex = AlphabetUpper.IndexOf(c);
+                int lowerIndex = AlphabetLower.IndexOf(c);
+
+                if (upperIndex >= 0)
+                {
+                    int newIndex = (upperIndex + shift + AlphabetUpper.Length) % AlphabetUpper.Length;
+                    if (newIndex < 0) newIndex += AlphabetUpper.Length;
+                    result.Append(AlphabetUpper[newIndex]);
+                }
+                else if (lowerIndex >= 0)
+                {
+                    int newIndex = (lowerIndex + shift + AlphabetLower.Length) % AlphabetLower.Length;
+                    if (newIndex < 0) newIndex += AlphabetLower.Length;
+                    result.Append(AlphabetLower[newIndex]);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+    public class BCipher : ICipher
+    {
+        private const string AlphabetUpper = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";
+        private const string AlphabetLower = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+
+        public string encode(string input)
+        {
+            return Transform(input);
+        }
+
+        public string decode(string input)
+        {
+            return Transform(input);
+        }
+
+        private string Transform(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                int upperIndex = AlphabetUpper.IndexOf(c);
+                int lowerIndex = AlphabetLower.IndexOf(c);
+
+                if (upperIndex >= 0)
+                {
+                    result.Append(AlphabetUpper[AlphabetUpper.Length - 1 - upperIndex]);
+                }
+                else if (lowerIndex >= 0)
+                {
+                    result.Append(AlphabetLower[AlphabetLower.Length - 1 - lowerIndex]);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            ICipher aCipher = new ACipher();
+            string original = "АБВГҐДЕЄЖЗИІЇЙК ЛМНОП РСТУФХЦЧШЩЬЮЯ";
+
+            Console.WriteLine("ACipher Шифрування:");
+            string encodedA = aCipher.encode(original);
+            Console.WriteLine("Оригінал: " + original);
+            Console.WriteLine("Зашифровано: " + encodedA);
+
+            Console.WriteLine("\nACipher Дешифрування:");
+            string decodedA = aCipher.decode(encodedA);
+            Console.WriteLine("Зашифрований текст: " + encodedA);
+            Console.WriteLine("Розшифровано: " + decodedA);
+
+            ICipher bCipher = new BCipher();
+            Console.WriteLine("\n\nBCipher Шифрування:");
+            string encodedB = bCipher.encode(original);
+            Console.WriteLine("Оригінал: " + original);
+            Console.WriteLine("Зашифровано: " + encodedB);
+
+            Console.WriteLine("\nBCipher Дешифрування:");
+            string decodedB = bCipher.decode(encodedB);
+            Console.WriteLine("Зашифрований текст: " + encodedB);
+            Console.WriteLine("Розшифровано: " + decodedB);
+
+            Console.WriteLine("\nСпеціальні тести для BCipher:");
+            Console.WriteLine("'А' = '" + bCipher.encode("А") + "'");
+            Console.WriteLine("'Б' = '" + bCipher.encode("Б") + "'");
+            Console.WriteLine("'Ґ' = '" + bCipher.encode("Ґ") + "'");
+            Console.WriteLine("'а' = '" + bCipher.encode("а") + "'");
+            Console.WriteLine("'є' = '" + bCipher.encode("є") + "'");
+        }
+    }
+}
